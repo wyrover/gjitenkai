7a6dcf8f2308a16ef8bfd39bd6ad25d1b77d7c88
olivier DREVET <odrevet@gmail.com>
2015-06-18 08:50:23 +0200

    test if reading definition. comments

--- test/parse_line.c
+++ test/parse_line.c
@@ -1,22 +1,24 @@
-//gcc parse_line.c $(pkg-config --cflags --libs gtk+-3.0) -I../src/worddic/ ../src/worddic/dicentry.c -o parse_line
-
+//gcc parse_line.c ../src/worddic/dicentry.c $(pkg-config --cflags --libs gtk+-3.0) -I../src/worddic/ -o parse_line
 #include <gtk/gtk.h>
 
 #include "../src/worddic/dicentry.h"
 
+
+#define LINE "チェイサー  /(n) (1) chaser/pursuer/(2) chaser/light drink (often just water) taken after a strong alcoholic drink/"
+
 int main( int argc, char **argv )
 {
 
-  const gchar *line = "凜 [りん] /(adj-t,adv-to) (1) cold/frigid/bracing/(2) dignified/";
+  //const gchar *line = "凜 [りん] /(adj-t,adv-to) (1) cold/frigid/bracing/(2) dignified/";
   
-  //const gchar *line = "チェイサー  /(n) (1) chaser/pursuer/(2) chaser/light drink (often just water) taken after a strong alcoholic drink/";
+  const gchar const *line = LINE;
 
   //const gchar *line = "強盗返し [がんとうがえし] /(oK) (n) rotating stage machinery, which rolls backwards 90 degrees to reveal the next scene/";
   
   GjitenDicentry* entry = parse_line(line);
   
   g_printf("Jap definition: %s\n", entry->jap_definition);
-  g_printf("Jap reading: %s\n", entry->jap_reading);
+  if(entry->jap_reading)g_printf("Jap reading: %s\n", entry->jap_reading);
   g_printf("Definitions: \n");
 
   GList *d = NULL;
@@ -25,6 +27,14 @@ int main( int argc, char **argv )
       d = d->next){
     g_printf("%s\n", d->data);
     
+    }
+
+
+  g_printf("original line was %s\n", line);
+
+  if(strcmp(line, LINE)){
+    g_printf("WARNING: the line was modified\n");
   }
+  
   return 1;
 }
