1bad168961b295e01c97c41904dc7a9a1c699a79
olivier DREVET <odrevet@gmail.com>
2015-06-25 21:04:56 +0200

    removed obsolet search function from gjiten

--- src/worddic/worddic_dicfile.h
+++ src/worddic/worddic_dicfile.h
@@ -28,9 +28,4 @@ GList *worddic_dicfile_parse(GjitenDicfile *dicfile);
 GList *dicfile_search_regex(WorddicDicfile *dicfile,
 			    const gchar *srchstrg_regex,
 			    GList **matched_part);
-
-GList *dicfile_search(GjitenDicfile *dicfile, const gchar *srchstrg, 
-                      gint match_criteria_jp, gint match_criteria_lat, 
-                      gint match_type);
-
 #endif
