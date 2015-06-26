18e6d9082b055bcf173caa5e07c32520bfda9caa
olivier DREVET <odrevet@gmail.com>
2015-06-26 19:17:29 +0200

    moved a loop and a condition inside-out for better performances

--- src/worddic/worddic_dicfile.c
+++ src/worddic/worddic_dicfile.c
@@ -32,33 +32,37 @@ GList *dicfile_search(WorddicDicfile *dicfile, const gchar *srchstrg_regex){
   GMatchInfo *match_info;
 
   GRegex* regex = g_regex_new (srchstrg_regex,
-				G_REGEX_OPTIMIZE,
-				0,
-				&error);
+                               G_REGEX_OPTIMIZE,
+                               0,
+                               &error);
 
   if(!regex)return NULL;
   
   //detect is the search expression is in japanese or latin char
   gboolean jpsrch = detect_japanese(srchstrg_regex);
 
-  GList* list_dicentry = NULL;
-  for(list_dicentry = dicfile->entries;
-      list_dicentry != NULL;
-      list_dicentry = list_dicentry->next){
+  if(jpsrch){
+    //if the search expression contains at least a japanese character,
+    //search matches in the japanese definition or japanese reading
 
-    GjitenDicentry* dicentry = list_dicentry->data;
-      
-    if(jpsrch){
-      //if the search expression contains at least a japanese character,
-      //search matches in the japanese definition or japanese reading
+    GList* list_dicentry = NULL;
+    for(list_dicentry = dicfile->entries;
+        list_dicentry != NULL;
+        list_dicentry = list_dicentry->next){
+
+      GjitenDicentry* dicentry = list_dicentry->data;
+
       GList *jap_definition = dicentry->jap_definition;
+
+      //search in the definition
       while(jap_definition != NULL){
         match = g_regex_match (regex, jap_definition->data, 0, &match_info);
 
         if(match)break;
         else jap_definition = jap_definition->next; 
       }
-      
+
+      //if no match in the definition, search in the reading
       if(!match && dicentry->jap_reading){
         GList *jap_reading = dicentry->jap_reading;
         while(jap_reading != NULL){
@@ -68,8 +72,30 @@ GList *dicfile_search(WorddicDicfile *dicfile, const gchar *srchstrg_regex){
           else jap_reading = jap_reading->next; 
         }
       }
+
+      //if there is a match, copy the entry into the result list
+      if(match){
+        //fetch the matched string
+        gchar *word = g_match_info_fetch (match_info, 0);
+
+        //create a new dicresult struct with the entry and the match
+        dicresult *p_dicresult = g_new0(dicresult, 1);
+        p_dicresult->match = word;
+        p_dicresult->entry = dicentry;
+      
+        //add the dicentry in the result list
+        results = g_list_append(results, p_dicresult);
+      }
     }
-    else{
+  }
+  else{
+    GList* list_dicentry = NULL;
+    for(list_dicentry = dicfile->entries;
+        list_dicentry != NULL;
+        list_dicentry = list_dicentry->next){
+
+      GjitenDicentry* dicentry = list_dicentry->data;
+
       //if there is no japanese characters, search matches in the gloss
       GList *gloss = dicentry->gloss;
       while(gloss != NULL){
@@ -77,21 +103,21 @@ GList *dicfile_search(WorddicDicfile *dicfile, const gchar *srchstrg_regex){
 
         if(match)break;
         else gloss = gloss->next; 
-        }
-    }
-    
-    //if there is a match, copy the entry into the result list
-    if(match){
-      //fetch the matched string
-      gchar *word = g_match_info_fetch (match_info, 0);
+      }
 
-      //create a new dicresult struct with the entry and the match
-      dicresult *p_dicresult = g_new0(dicresult, 1);
-      p_dicresult->match = word;
-      p_dicresult->entry = dicentry;
+      //if there is a match, copy the entry into the result list
+      if(match){
+        //fetch the matched string
+        gchar *word = g_match_info_fetch (match_info, 0);
+
+        //create a new dicresult struct with the entry and the match
+        dicresult *p_dicresult = g_new0(dicresult, 1);
+        p_dicresult->match = word;
+        p_dicresult->entry = dicentry;
       
-      //add the dicentry in the result list
-      results = g_list_append(results, p_dicresult);
+        //add the dicentry in the result list
+        results = g_list_append(results, p_dicresult);
+      }
     }
   }
 
