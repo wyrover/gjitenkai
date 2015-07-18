8caeccf11775c71b495410b7a9c3d0b5ef1690c7
olivier DREVET <odrevet@gmail.com>
2015-07-18 15:00:07 +0200

    fix crash when updating radical button sensitivity (due to previous error when removing compiler warnings)

--- src/kanjidic/radical_list.c
+++ src/kanjidic/radical_list.c
@@ -19,8 +19,8 @@ void radical_list_init(kanjidic *kanjidic){
        radical_list != NULL;
        radical_list = g_list_next(radical_list)) {
      
-    const gchar* radical      = (const gchar*)((RadInfo*)radical_list->data)->radical;
-    gint strokes_count  = ((RadInfo*)radical_list->data)->strokes;
+    const gchar* radical = (const gchar*)((RadInfo*)radical_list->data)->radical;
+    gint strokes_count = ((RadInfo*)radical_list->data)->strokes;
 
     //the stroke count has change: display a label with the new count and
     //update the last stroke count
@@ -57,7 +57,7 @@ void radical_list_init(kanjidic *kanjidic){
     i++;
     if(i%RADICAL_PER_ROW == 0){j++;i=0;}
 
-    }
+  }
 }
 
 void radical_list_update_sensitivity(kanjidic *kanjidic){
@@ -68,42 +68,43 @@ void radical_list_update_sensitivity(kanjidic *kanjidic){
   const gchar *radicals = gtk_entry_get_text(entry_filter_radical);
 
   //point to the head of the button list
-  GList *l;
+  GList *l=button_list;
   
   //if no radicals, set all buttons sensitivity to true
   if(!strcmp(radicals, "")){
-    for (l = button_list;l != NULL;l = g_list_next(l)) {
+    for (;
+         l != NULL;
+         l = g_list_next(l)) {
       gtk_widget_set_sensitive(GTK_WIDGET(l->data), TRUE);
     }
-    return;
   }
-  
-  //for every radicals, ckeck if there at least a match with the current 
-  //entered radicals and the kanji button
-  do{
-    //get the current button and it's kanji 
-    GtkButton* button = (GtkButton*)l->data;
-    const gchar* cur_radical = gtk_button_get_label(button);
-    
-    //append the current radical to the filter entry radicals text 
-    gchar* srch = g_new0(gchar, strlen(radicals) + strlen(cur_radical) + 1);
-    g_strlcpy(srch, radicals, strlen(radicals) + strlen(cur_radical));
-    strcat (srch, cur_radical);
-    
-    //if no match, set the sensitivity to false on this button
-    gboolean sensitivity; 
-    //TODO? create a faster function that return a boolean on the first match
-    //(seems fast enought with this function but can be optimised)
-    if(get_kanji_by_radical(srch, kanjidic->rad_info_hash) == NULL){
-      sensitivity = FALSE;
-    }
-    else{
-      sensitivity = TRUE;
-    }
-    
-    gtk_widget_set_sensitive(GTK_WIDGET(button), sensitivity);
+  else{
+    //for every radicals, ckeck if there at least a match with the current 
+    //entered radicals and the kanji button
+    do{
+      //get the current button and it's kanji 
+      GtkButton* button = (GtkButton*)l->data;
+      const gchar* cur_radical = gtk_button_get_label(button);
 
-    g_free(srch);
-  }while((l = g_list_next(l)));
+      //append the current radical to the filter entry radicals text 
+      gchar* srch = g_new0(gchar, strlen(radicals) + strlen(cur_radical) + 1);
+      g_strlcpy(srch, radicals, strlen(radicals) + strlen(cur_radical));
+      strcat (srch, cur_radical);
+    
+      //if no match, set the sensitivity to false on this button
+      gboolean sensitivity; 
+      //TODO? create a faster function that return a boolean on the first match
+      //(seems fast enought with this function but can be optimised)
+      if(get_kanji_by_radical(srch, kanjidic->rad_info_hash) == NULL){
+        sensitivity = FALSE;
+      }
+      else{
+        sensitivity = TRUE;
+      }
+    
+      gtk_widget_set_sensitive(GTK_WIDGET(button), sensitivity);
 
+      g_free(srch);
+    }while((l = g_list_next(l)));
+  }
 }
