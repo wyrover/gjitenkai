b6aeed3512ff4da070009ee825871d63f71ae1c0
olivier DREVET <odrevet@gmail.com>
2015-07-22 01:08:24 +0200

    test routine to check worddic_dicfile

---
+++ test/dicfile.c
@@ -0,0 +1,15 @@
+//gcc -g dicfile.c ../src/worddic/worddic_dicfile.c ../src/worddic/dicentry.c ../src/common/dicutil.c ../src/worddic/gloss.c $(pkg-config --cflags --libs gtk+-3.0) -I../src/worddic/ -o dicfile
+#include <gtk/gtk.h>
+
+#include "../src/worddic/worddic_dicfile.h"
+
+int main( int argc, char **argv )
+{
+  WorddicDicfile *dicfile = g_new0(WorddicDicfile, 1);
+  dicfile->path = g_strdup(argv[1]);
+  worddic_dicfile_open(dicfile);
+  worddic_dicfile_parse_all(dicfile);
+  worddic_dicfile_close(dicfile);
+  worddic_dicfile_free(dicfile);
+  return 1;
+}
