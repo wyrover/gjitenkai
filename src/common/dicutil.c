44ce6a95332aed547e9d4c02f03530d8c5ce1271
olivier DREVET <odrevet@gmail.com>
2015-07-21 11:32:04 +0200

    rewrited getline function using fgets internaly

--- src/common/dicutil.c
+++ src/common/dicutil.c
@@ -302,57 +302,19 @@ char* strtok_r(
     return ret;
 }
 
-size_t getline(char **lineptr, size_t *n, FILE *stream) {
-    char *bufptr = NULL;
-    char *p = bufptr;
-    size_t size;
-    int c;
+#ifdef BUFSIZ
+#undef BUFSIZ
+#endif
+#define BUFSIZ 1600
 
-    if (lineptr == NULL) {
-    	return -1;
-    }
-    if (stream == NULL) {
-    	return -1;
-    }
-    if (n == NULL) {
-    	return -1;
-    }
-    bufptr = *lineptr;
-    size = *n;
-
-    c = fgetc(stream);
-    if (c == EOF) {
-    	return -1;
-    }
-    if (bufptr == NULL) {
-    	bufptr = malloc(128);
-    	if (bufptr == NULL) {
-    		return -1;
-    	}
-    	size = 128;
-    }
-    p = bufptr;
-    while(c != EOF) {
-    	if ((p - bufptr) > (size - 1)) {
-    		size = size + 128;
-    		bufptr = realloc(bufptr, size);
-    		if (bufptr == NULL) {
-    			return -1;
-    		}
-    	}
-    	*p++ = c;
-    	if (c == '\n') {
-    		break;
-    	}
-    	c = fgetc(stream);
-    }
-
-    *p++ = '\0';
-    *lineptr = bufptr;
-    *n = size;
-
-    return p - bufptr - 1;
+size_t getline(char** lineptr, size_t *n, FILE *stream){
+  if(!*lineptr)*lineptr = malloc(BUFSIZ * sizeof(char));
+  char *has_more = fgets (*lineptr, BUFSIZ, stream);
+  if(!has_more)return -1;
+  *n = strlen(*lineptr);
+  return *n-1;
 }
+
 
 gchar *path_relative(gchar *path){
 	gchar buffer[MAX_PATH];
