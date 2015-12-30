dd716e57a8cc3773a1d4dc1d97fffde7357bc536
Olivier DREVET <odrevet@gmail.com>
2015-12-30 11:08:15 +0000

    comments

--- test/dicfile_search.c
+++ test/dicfile_search.c
@@ -1,4 +1,4 @@
-//gcc -g dicfile_search.c ../src/worddic/worddic_dicfile.c ../src/worddic/dicentry.c ../src/common/dicutil.c ../src/worddic/gloss.c ../src/worddic/dicresult.c ./print_entry.c $(pkg-config --cflags --libs gtk+-3.0) -I../src/worddic/ -o dicfile_search
+//gcc -g dicfile_search.c ../src/worddic/worddic_dicfile.c ../src/worddic/dicentry.c ../src/common/dicutil.c ../src/worddic/gloss.c ../src/worddic/dicresult.c ./print_entry.c $(pkg-config --cflags --libs gtk+-3.0) -lz -I../src/worddic/ -o dicfile_search
 #include <gtk/gtk.h>
 
 #include "../src/worddic/worddic_dicfile.h"
@@ -15,8 +15,8 @@ int main( int argc, char **argv )
 {
   GList *results = NULL;
   
-  g_printf("parameters are dictionary path and search expression\n\
-parameters are not checked\n");
+  g_printf("parameters are: 'dictionary path' and 'search expression'\n\
+(parameters are not checked)\n");
   
   //create and open dictionary
   WorddicDicfile *dicfile = g_new0(WorddicDicfile, 1);
@@ -29,54 +29,56 @@ parameters are not checked\n");
   //close dicrionary
   worddic_dicfile_close(dicfile);
 
+  search_expression search_expr;
+  search_expr.search_criteria_jp = ANY_MATCH;
+  search_expr.search_criteria_lat = ANY_MATCH;
+  
   //kana
   //search hiragana on katakana
   if(hasKatakanaString(argv[2])) {
     gchar *hiragana = kata_to_hira(argv[2]);
+    search_expr.search_text = hiragana;
+
     results = g_list_concat(results, dicfile_search(dicfile,
-                                                    hiragana,
-                                                    "from katakana",
-                                                    GIALL,
-                                                    ANY_MATCH,
-                                                    ANY_MATCH,
-                                                    1)
-                            );
+						    &search_expr,
+						    "from katakana",
+						    GIALL,
+						    1)
+			    );
     g_free(hiragana);  //free memory
   }
   ///////////////////////////////////
-
-  
-  //search kata
-  if(hasHiraganaString(argv[2])) { 
+  //search katakana on hiragana
+  if (hasHiraganaString(argv[2])) { 
     gchar *katakana = hira_to_kata(argv[2]);
+    search_expr.search_text = katakana;
+        
     results = g_list_concat(results, dicfile_search(dicfile,
-                                                    katakana,
-                                                    "from hiragana",
-                                                    GIALL,
-                                                    ANY_MATCH,
-                                                    ANY_MATCH,
-                                                    1)
-                            );
+						    &search_expr,
+						    "from hiragana",
+						    GIALL,
+						    1)
+			    );
     g_free(katakana); //free memory
   }
+
   ////////////////////////////////
   
-  //search 
-  results = g_list_concat(results, dicfile_search(dicfile,
-                                                  argv[2],
-                                                  NULL,
-                                                  GIALL,
-                                                  ANY_MATCH,
-                                                  ANY_MATCH,
-                                                  -1)
-                          );
+    //standard search
+    search_expr.search_text = argv[2];
+    results = g_list_concat(results, dicfile_search(dicfile,
+                                                    &search_expr,
+                                                    NULL,
+                                                    GIALL,
+                                                    1)
+                            );
   //print
   GList *l;
   for(l=results;l!=NULL;l = l->next){
     dicresult *result = l->data;
     
     //print matched part and comment
-    g_printf("(%s) %s:\n", result->comment, result->match);
+    g_printf("COMMENT %s\n MATCH %s:\n", result->comment, result->match);
     //print the entry
     print_entry(result->entry);
   }
