a56e185a7133cfd16d1181a3a426677f6eb87130
odrevet <olivier.drevet@cycleco.eu>
2017-04-18 15:58:08 +0200

    [WIP] download the gz edict dictionary from monash ftp.

--- src/gjitenkai/callbacks.c
+++ src/gjitenkai/callbacks.c
@@ -8,7 +8,7 @@ static gboolean on_eval_expend_bushuu (const GMatchInfo *info,
 				       gpointer         data){
   kanjidic *kanjidic = data;
   gchar *match;
-  
+
   match = g_match_info_fetch (info, 0);
 
   GSList *kanji_by_radical_list=NULL;
@@ -20,12 +20,12 @@ static gboolean on_eval_expend_bushuu (const GMatchInfo *info,
     kanji_list = g_string_append (kanji_list, (gchar*)l->data);
   }
   kanji_list = g_string_append_c (kanji_list, ']');
-    
+
   res = g_string_append (res, kanji_list->str);
 
   g_string_free(kanji_list, TRUE);
   g_free (match);
-  
+
   return FALSE;
 }
 
@@ -39,7 +39,7 @@ G_MODULE_EXPORT void on_gjitenkai_search_expression_activate(GtkEntry *entry,
   if(!strcmp(search_entry_text, ""))return;
 
   //search for radicals in pairs of bracets (or fullwidth bracets) and replace
-  //it with a list of kanji in a square bracets [] alternative operator  
+  //it with a list of kanji in a square bracets [] alternative operator
   GRegex *regex_bushuu_delim = g_regex_new ("[<＜].+[＞>]",
 					    G_REGEX_UNGREEDY,
 					    0,
@@ -53,7 +53,7 @@ G_MODULE_EXPORT void on_gjitenkai_search_expression_activate(GtkEntry *entry,
   //free memory
   g_regex_unref(regex_bushuu_delim);
 
-  //search in worrdic 
+  //search in worrdic
   gboolean has_match = worddic_search(search_text,
 				      gjitenkai->worddic);
 
@@ -69,7 +69,7 @@ G_MODULE_EXPORT void on_gjitenkai_search_expression_activate(GtkEntry *entry,
 
 //History
 ////Clear
-G_MODULE_EXPORT void on_gjitenkai_history_clear_activate (GtkMenuItem *menuitem, 
+G_MODULE_EXPORT void on_gjitenkai_history_clear_activate (GtkMenuItem *menuitem,
                                                 gjitenkai *gjitenkai){
   //free the history list
   g_slist_free_full(gjitenkai->worddic->conf->history, (GDestroyNotify)g_free);
@@ -107,7 +107,7 @@ G_MODULE_EXPORT void on_gjitenkai_menuitem_history_click(GtkWidget *menuitem_his
 G_MODULE_EXPORT void on_gjitenkai_menuitem_prefs_activate(GtkButton *button,
                                                           gjitenkai *gjitenkai){
   //set size and display the preference window
-  GtkDialog *prefs = (GtkDialog*)gtk_builder_get_object(gjitenkai->definitions, 
+  GtkDialog *prefs = (GtkDialog*)gtk_builder_get_object(gjitenkai->definitions,
                                                                "prefs");
   gtk_window_set_default_size(GTK_WINDOW(prefs), 420, 280);
   gtk_widget_show_all ((GtkWidget*)prefs);
@@ -115,7 +115,7 @@ G_MODULE_EXPORT void on_gjitenkai_menuitem_prefs_activate(GtkButton *button,
 
 G_MODULE_EXPORT void on_gjitenkai_button_prefs_OK_clicked(GtkButton *button,
                                                           gjitenkai *gjitenkai){
- GtkDialog *prefs = (GtkDialog*)gtk_builder_get_object(gjitenkai->definitions, 
+ GtkDialog *prefs = (GtkDialog*)gtk_builder_get_object(gjitenkai->definitions,
                                                                "prefs");
  gtk_widget_hide(GTK_WIDGET(prefs));
 }
@@ -123,9 +123,9 @@ G_MODULE_EXPORT void on_gjitenkai_button_prefs_OK_clicked(GtkButton *button,
 G_MODULE_EXPORT void on_menuitem_view_worddic_toggled(GtkCheckMenuItem *menu_item,
                                                       gjitenkai *gjitenkai){
   GtkWidget *box_worddic = (GtkWidget*)
-    gtk_builder_get_object(gjitenkai->worddic->definitions, 
+    gtk_builder_get_object(gjitenkai->worddic->definitions,
                            "box_toplevel");
-  
+
   if(gtk_check_menu_item_get_active(menu_item)){
     gtk_widget_show(box_worddic);
   }
@@ -135,8 +135,8 @@ G_MODULE_EXPORT void on_menuitem_view_worddic_toggled(GtkCheckMenuItem *menu_ite
 }
 
 G_MODULE_EXPORT void on_menuitem_view_kanjidic_toggled(GtkMenuItem *menu_item, gjitenkai *gjitenkai){
-  GtkWidget *box_kanjidic = (GtkWidget*)gtk_builder_get_object(gjitenkai->kanjidic->definitions, 
-							       "box_toplevel");  
+  GtkWidget *box_kanjidic = (GtkWidget*)gtk_builder_get_object(gjitenkai->kanjidic->definitions,
+							       "box_toplevel");
   if(gtk_check_menu_item_get_active(GTK_CHECK_MENU_ITEM(menu_item))){
     gtk_widget_show(box_kanjidic);
   }
@@ -146,29 +146,29 @@ G_MODULE_EXPORT void on_menuitem_view_kanjidic_toggled(GtkMenuItem *menu_item, g
 }
 
 G_MODULE_EXPORT void on_menuitem_paned_toggled(GtkMenuItem *menu_item, gjitenkai *gjitenkai){
-  GtkWidget *box_worddic = (GtkWidget*)gtk_builder_get_object(gjitenkai->worddic->definitions, 
+  GtkWidget *box_worddic = (GtkWidget*)gtk_builder_get_object(gjitenkai->worddic->definitions,
                                                               "box_toplevel");
-  GtkWidget *box_kanjidic = (GtkWidget*)gtk_builder_get_object(gjitenkai->kanjidic->definitions, 
+  GtkWidget *box_kanjidic = (GtkWidget*)gtk_builder_get_object(gjitenkai->kanjidic->definitions,
                                                                "box_toplevel");
-  GtkWindow *box_top = (GtkWindow*)gtk_builder_get_object(gjitenkai->definitions, 
-                                                          "box_top");  
+  GtkWindow *box_top = (GtkWindow*)gtk_builder_get_object(gjitenkai->definitions,
+                                                          "box_top");
   //remove the worddic and kanjidic boxes from the notebook pages'box
   GtkWidget *page_worddic = gtk_notebook_get_nth_page(gjitenkai->notebook, 0);
   GtkWidget *page_kanjidic = gtk_notebook_get_nth_page(gjitenkai->notebook, 1);
-  
+
   g_object_ref(box_worddic);
-  g_object_ref(box_kanjidic);  
+  g_object_ref(box_kanjidic);
   gtk_container_remove (GTK_CONTAINER(page_worddic), GTK_WIDGET(box_worddic));
   gtk_container_remove (GTK_CONTAINER(page_kanjidic), GTK_WIDGET(box_kanjidic));
-  
+
   //remove the notebook from the window top box
   g_object_ref(gjitenkai->notebook);
   gtk_container_remove (GTK_CONTAINER(box_top), GTK_WIDGET(gjitenkai->notebook));
 
   //add worddic and kanjidic to the pane
   gtk_paned_add1(gjitenkai->paned, box_worddic);
-  gtk_paned_add2(gjitenkai->paned, box_kanjidic);  
-  
+  gtk_paned_add2(gjitenkai->paned, box_kanjidic);
+
   //add the pane to the box top
   gtk_box_pack_start(GTK_BOX(box_top), GTK_WIDGET(gjitenkai->paned), TRUE, TRUE, 0);
 
@@ -178,12 +178,12 @@ G_MODULE_EXPORT void on_menuitem_paned_toggled(GtkMenuItem *menu_item, gjitenkai
 
 G_MODULE_EXPORT void on_menuitem_notebook_toggled(GtkMenuItem *menu_item, gjitenkai *gjitenkai){
 
-  GtkWidget *box_worddic = (GtkWidget*)gtk_builder_get_object(gjitenkai->worddic->definitions, 
+  GtkWidget *box_worddic = (GtkWidget*)gtk_builder_get_object(gjitenkai->worddic->definitions,
                                                               "box_toplevel");
-  GtkWidget *box_kanjidic = (GtkWidget*)gtk_builder_get_object(gjitenkai->kanjidic->definitions, 
+  GtkWidget *box_kanjidic = (GtkWidget*)gtk_builder_get_object(gjitenkai->kanjidic->definitions,
                                                                "box_toplevel");
-  GtkWindow *box_top = (GtkWindow*)gtk_builder_get_object(gjitenkai->definitions, 
-                                                          "box_top");  
+  GtkWindow *box_top = (GtkWindow*)gtk_builder_get_object(gjitenkai->definitions,
+                                                          "box_top");
 
   //remove the worddic and kanjidic from the pane
   g_object_ref(box_worddic);
@@ -197,28 +197,28 @@ G_MODULE_EXPORT void on_menuitem_notebook_toggled(GtkMenuItem *menu_item, gjiten
 
   gtk_box_pack_start(GTK_BOX(page_worddic), box_worddic, TRUE, TRUE, 0);
   gtk_box_pack_start(GTK_BOX(page_kanjidic), box_kanjidic, TRUE, TRUE, 0);
-  
+
   //remove the pane from the window top box
   g_object_ref(gjitenkai->paned);
   gtk_container_remove (GTK_CONTAINER(box_top), GTK_WIDGET(gjitenkai->paned));
-    
+
   //add the notebook to the top box
   gtk_box_pack_start(GTK_BOX(box_top), GTK_WIDGET(gjitenkai->notebook), TRUE, TRUE, 0);
-  
+
   gtk_widget_show_all(GTK_WIDGET(box_top));
 }
 
 //about
-G_MODULE_EXPORT void on_gjiten_menuitem_help_about_activate(GtkMenuItem *menuitem, 
+G_MODULE_EXPORT void on_gjiten_menuitem_help_about_activate(GtkMenuItem *menuitem,
                                                               gjitenkai *gjitenkai){
   GtkWindow *window_about = (GtkWindow*)gtk_builder_get_object(gjitenkai->definitions,
                                                                "aboutdialog");
   gtk_dialog_run(GTK_DIALOG(window_about));
-  gtk_widget_hide (GTK_WIDGET(window_about)); 
+  gtk_widget_hide (GTK_WIDGET(window_about));
 }
 
 //hide and prevent deletion
-G_MODULE_EXPORT gboolean on_gjitenkai_prefs_delete_event(GtkWindow *window, 
+G_MODULE_EXPORT gboolean on_gjitenkai_prefs_delete_event(GtkWindow *window,
                                                          gjitenkai *gjitenkai) {
   gtk_widget_hide(GTK_WIDGET(window));
   return TRUE;
@@ -236,8 +236,8 @@ G_MODULE_EXPORT gboolean on_gjitenkai_search_results_button_release_event(GtkWid
   gunichar kanji;
 
   if (event->button != 1) return FALSE;
-  
-  gtk_text_view_window_to_buffer_coords(GTK_TEXT_VIEW (text_view), 
+
+  gtk_text_view_window_to_buffer_coords(GTK_TEXT_VIEW (text_view),
 					GTK_TEXT_WINDOW_WIDGET,
 					event->x, event->y, &x, &y);
 
@@ -252,3 +252,8 @@ G_MODULE_EXPORT gboolean on_gjitenkai_search_results_button_release_event(GtkWid
   return FALSE;
 }
 
+G_MODULE_EXPORT void on_download_dictionary_activate(GtkMenuItem *menu_item, gjitenkai *gjitenkai){
+  GtkWidget *box_kanjidic = (GtkWidget*)gtk_builder_get_object(gjitenkai->kanjidic->definitions,
+							       "box_toplevel");
+  printf("Activated download");
+}
--- src/worddic/callbacks.c
+++ src/worddic/callbacks.c
@@ -1,5 +1,6 @@
 #include <glib.h>
 #include <glib/gprintf.h>
+#include <libsoup/soup.h>
 
 #include "worddic.h"
 #include "worddic_dicfile.h"
@@ -7,7 +8,7 @@
 #include "../common/dicfile.h"
 
 /*
- * Update the cursor image if the pointer is above a kanji. 
+ * Update the cursor image if the pointer is above a kanji.
  */
 G_MODULE_EXPORT gboolean on_search_results_motion_notify_event(GtkWidget *text_view,
                                                                GdkEventMotion *event){
@@ -15,15 +16,15 @@ G_MODULE_EXPORT gboolean on_search_results_motion_notify_event(GtkWidget *text_v
   GtkTextIter mouse_iter;
   gunichar kanji;
   gint trailing;
-  
-  gtk_text_view_window_to_buffer_coords(GTK_TEXT_VIEW(text_view), 
+
+  gtk_text_view_window_to_buffer_coords(GTK_TEXT_VIEW(text_view),
                                         GTK_TEXT_WINDOW_WIDGET,
                                         event->x, event->y, &x, &y);
-  
+
   gtk_text_view_get_iter_at_position(GTK_TEXT_VIEW(text_view),
                                      &mouse_iter, &trailing,
                                      x , y);
-  
+
   kanji = gtk_text_iter_get_char(&mouse_iter);
 
   // Change the cursor if necessary
@@ -37,7 +38,7 @@ G_MODULE_EXPORT gboolean on_search_results_motion_notify_event(GtkWidget *text_v
                                                    GTK_TEXT_WINDOW_TEXT),
                           cursor_default);
   }
-  
+
   return FALSE;
 }
 
@@ -61,7 +62,7 @@ G_MODULE_EXPORT void on_search_expression_activate(GtkEntry *entry, worddic *wor
 //Edit
 ///Preferences
 G_MODULE_EXPORT void on_menuitem_prefs_activate(GtkMenuItem *menuitem, worddic *worddic){
-  GtkDialog *prefs = GTK_DIALOG(gtk_builder_get_object(worddic->definitions, 
+  GtkDialog *prefs = GTK_DIALOG(gtk_builder_get_object(worddic->definitions,
                                                        "prefs"));
   //set size and display the preference window
   gtk_window_set_default_size(GTK_WINDOW(prefs), 320, 220);
@@ -70,38 +71,38 @@ G_MODULE_EXPORT void on_menuitem_prefs_activate(GtkMenuItem *menuitem, worddic *
 
 //Search
 ///Japanese
-G_MODULE_EXPORT void on_menuitem_search_japanese_exact_activate (GtkMenuItem *menuitem, 
+G_MODULE_EXPORT void on_menuitem_search_japanese_exact_activate (GtkMenuItem *menuitem,
                                                                  worddic *worddic){
   worddic->match_criteria_jp = EXACT_MATCH;
 }
 
-G_MODULE_EXPORT void on_menuitem_search_japanese_start_activate (GtkMenuItem *menuitem, 
+G_MODULE_EXPORT void on_menuitem_search_japanese_start_activate (GtkMenuItem *menuitem,
                                                                  worddic *worddic){
   worddic->match_criteria_jp = START_WITH_MATCH;
 }
 
-G_MODULE_EXPORT void on_menuitem_search_japanese_end_activate (GtkMenuItem *menuitem, 
+G_MODULE_EXPORT void on_menuitem_search_japanese_end_activate (GtkMenuItem *menuitem,
                                                                worddic *worddic){
   worddic->match_criteria_jp = END_WITH_MATCH;
 }
 
-G_MODULE_EXPORT void on_menuitem_search_japanese_any_activate (GtkMenuItem *menuitem, 
+G_MODULE_EXPORT void on_menuitem_search_japanese_any_activate (GtkMenuItem *menuitem,
                                                                worddic *worddic){
   worddic->match_criteria_jp = ANY_MATCH;
 }
 
 ///Latin
-G_MODULE_EXPORT void on_menuitem_search_latin_whole_expressions_activate (GtkMenuItem *menuitem, 
+G_MODULE_EXPORT void on_menuitem_search_latin_whole_expressions_activate (GtkMenuItem *menuitem,
                                                                           worddic *worddic){
   worddic->match_criteria_lat = EXACT_MATCH;
 }
 
-G_MODULE_EXPORT void on_menuitem_search_latin_whole_words_activate (GtkMenuItem *menuitem, 
+G_MODULE_EXPORT void on_menuitem_search_latin_whole_words_activate (GtkMenuItem *menuitem,
                                                                     worddic *worddic){
   worddic->match_criteria_lat = WORD_MATCH;
 }
 
-G_MODULE_EXPORT void on_menuitem_search_latin_any_matches_activate (GtkMenuItem *menuitem, 
+G_MODULE_EXPORT void on_menuitem_search_latin_any_matches_activate (GtkMenuItem *menuitem,
                                                                     worddic *worddic){
   worddic->match_criteria_lat = ANY_MATCH;
 }
@@ -109,31 +110,31 @@ G_MODULE_EXPORT void on_menuitem_search_latin_any_matches_activate (GtkMenuItem 
 
 //Help
 ///About
-G_MODULE_EXPORT void on_menuitem_help_about_activate (GtkMenuItem *menuitem, 
+G_MODULE_EXPORT void on_menuitem_help_about_activate (GtkMenuItem *menuitem,
                                                       worddic *worddic){
   GtkWindow *window_about = (GtkWindow*)gtk_builder_get_object(worddic->definitions,
                                                                "aboutdialog");
   gtk_dialog_run(GTK_DIALOG(window_about));
-  gtk_widget_hide (GTK_WIDGET(window_about)); 
+  gtk_widget_hide (GTK_WIDGET(window_about));
 }
 
 //History
 ////Clear
-G_MODULE_EXPORT void on_history_clear_activate (GtkMenuItem *menuitem, 
+G_MODULE_EXPORT void on_history_clear_activate (GtkMenuItem *menuitem,
                                                 worddic *worddic){
 }
 
-G_MODULE_EXPORT void on_checkbutton_dark_theme_toggled(GtkCheckButton* check_button,  
+G_MODULE_EXPORT void on_checkbutton_dark_theme_toggled(GtkCheckButton* check_button,
                                                        worddic *worddic){
   gboolean toggled = gtk_toggle_button_get_active((GtkToggleButton*)check_button);
 
   GtkSettings *settings = gtk_settings_get_default ();
-  
+
   g_object_set (G_OBJECT (settings),
                 "gtk-application-prefer-dark-theme",
                 toggled,
                 NULL);
-  
+
   worddic->conf->dark_theme = toggled;
   worddic_conf_save(worddic->settings, worddic->conf, WSE_DARK_THEME);
 }
@@ -146,8 +147,8 @@ void G_MODULE_EXPORT on_worddic_search_results_edge_reached(GtkScrolledWindow* s
 							    worddic* worddic){
 
   //get the search result text entry to display matches
-  GtkTextBuffer *textbuffer_search_results = 
-    (GtkTextBuffer*)gtk_builder_get_object(worddic->definitions, 
+  GtkTextBuffer *textbuffer_search_results =
+    (GtkTextBuffer*)gtk_builder_get_object(worddic->definitions,
                                            "textbuffer_search_results");
   if(pos == GTK_POS_BOTTOM){
     print_entries(textbuffer_search_results, worddic);
@@ -156,8 +157,29 @@ void G_MODULE_EXPORT on_worddic_search_results_edge_reached(GtkScrolledWindow* s
 
 #endif
 
+static SoupSession *session;
+static GMainLoop *loop;
+static gboolean debug, head, quiet;
+static const gchar *output_file_path = NULL;
+
+G_MODULE_EXPORT void on_button_download_clicked(GtkButton* button, worddic *p_worddic){
+  const char *download_url = "http://ftp.monash.edu/pub/nihongo/edict2u.gz";
+
+  session = soup_session_sync_new();
+  SoupMessage *msg;
+  msg = soup_message_new ("GET",  download_url);
+  soup_session_send_message (session, msg);
+
+  FILE* fh = g_open("edict2u.gz", O_WRONLY);
+
+  g_file_set_contents("EDICT.gz", msg->response_body->data, msg->response_body->length, NULL);
+
+  g_close(fh);
+}
+
+
 G_MODULE_EXPORT void on_button_welcome_clicked(GtkButton* button, worddic *p_worddic){
-  GtkDialog *dialog = (GtkDialog*)gtk_builder_get_object(p_worddic->definitions, 
+  GtkDialog *dialog = (GtkDialog*)gtk_builder_get_object(p_worddic->definitions,
                                                          "dialog_welcome");
-  gtk_widget_hide (GTK_WIDGET(dialog)); 
+  gtk_widget_hide (GTK_WIDGET(dialog));
 }
