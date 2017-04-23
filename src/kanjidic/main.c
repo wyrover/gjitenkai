b425df53b5b4540abf9696602977334d59fcce57
Olivier DREVET <odrevet@gmail.com>
2017-04-23 12:06:39 +0000

    removed unused code to load png icon (done by glade)

--- src/kanjidic/main.c
+++ src/kanjidic/main.c
@@ -17,13 +17,6 @@ int main( int argc, char **argv )
   GtkWindow *window = (GtkWindow*)gtk_builder_get_object(kanjidic.definitions,
                                                           "kanjidic");
 
-  //set icon
-  GdkPixbuf *pixbuf;
-  GError *error = NULL;
-  pixbuf = gdk_pixbuf_new_from_file("pixmaps/kai.png", &error);
-
-  gtk_window_set_icon(GTK_WINDOW(window), pixbuf);
-
   //box in the top of the window
   GtkWidget *box_top = (GtkWidget*)gtk_builder_get_object(kanjidic.definitions,
                                                               "box_top");
--- src/worddic/main.c
+++ src/worddic/main.c
@@ -15,37 +15,30 @@ int main( int argc, char **argv )
 
   gtk_init (&argc, &argv);
   worddic_init (&worddic);
-  
+
   //construct the worrdic main window
   //worddic window
-  GtkWindow *window = (GtkWindow*)gtk_builder_get_object(worddic.definitions, 
+  GtkWindow *window = (GtkWindow*)gtk_builder_get_object(worddic.definitions,
                                                           "worddic");
-  //set icon
-  GdkPixbuf *pixbuf;
-  GError *error = NULL;
-  pixbuf = gdk_pixbuf_new_from_file("pixmaps/kai.png", &error);
-   
-  gtk_window_set_icon(GTK_WINDOW(window), pixbuf);
-  
   //box at the top of the worddic window
-  GtkWidget *box_top = (GtkWidget*)gtk_builder_get_object(worddic.definitions, 
+  GtkWidget *box_top = (GtkWidget*)gtk_builder_get_object(worddic.definitions,
                                                               "box_top");
 
   //box at toplevel
-  GtkWidget *box_worddic = (GtkWidget*)gtk_builder_get_object(worddic.definitions, 
+  GtkWidget *box_worddic = (GtkWidget*)gtk_builder_get_object(worddic.definitions,
                                                               "box_toplevel");
-  
+
   gtk_box_pack_start(GTK_BOX(box_top), box_worddic, TRUE, TRUE, 0);
 
   gtk_window_set_default_size(GTK_WINDOW(window), 640, 320);
 
   //construct the worrdic prefs dialog
   //box of the prefs dialog
-  GtkWidget *dialog_vbox_prefs = (GtkWidget*)gtk_builder_get_object(worddic.definitions, 
+  GtkWidget *dialog_vbox_prefs = (GtkWidget*)gtk_builder_get_object(worddic.definitions,
                                                                     "dialog-vbox_prefs");
 
   //preferences box at toplevel
-  GtkWidget *box_toplevel_prefs = (GtkWidget*)gtk_builder_get_object(worddic.definitions, 
+  GtkWidget *box_toplevel_prefs = (GtkWidget*)gtk_builder_get_object(worddic.definitions,
                                                                      "box_toplevel_prefs");
   gtk_box_pack_start(GTK_BOX(dialog_vbox_prefs), box_toplevel_prefs, TRUE, TRUE, 0);
 
