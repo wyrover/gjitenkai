fc2184112c21cfdb0f4e2e94983fba03a230af90
odrevet <olivier.drevet@cycleco.eu>
2017-11-23 09:51:37 +0100

    sub_gloss struct

--- src/worddic/gloss.h
+++ src/worddic/gloss.h
@@ -3,20 +3,17 @@
 
 #include <gtk/gtk.h>
 
+//todo rename to gloss
+typedef struct sub_gloss_t{
+  gchar lang[3];   // ISO 639-2 defaults to eng
+  gchar *content;
+}sub_gloss;
+
 //todo rename to sense
 typedef struct gloss_t{
-  //todo rename to gloss
-  GSList *sub_gloss;    //glosses inside the gloss
-
-  //general informations
-  //in plain text (to display)
-  GSList *general_informations;
-
-  //in a bits field (to search)
-  gint GI:3;
-
-  // ISO 639-2 defaults to eng
-  gchar lang[3];  //todo but in sub gloss (renamed to gloss)
+  GSList *sub_gloss;             //sub gloss
+  GSList *general_informations;  //General Infor in plain text (to display)
+  gint GI:3;                     //in a bits field (to search)
 }gloss;
 
 void gloss_free(gloss *p_gloss);
