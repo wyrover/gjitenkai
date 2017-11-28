3ae731773718bf7be5cb02877154a819a645872c
odrevet <olivier.drevet@cycleco.eu>
2017-11-28 09:00:08 +0100

    search css file in XDG directories

--- src/gjitenkai/main.c
+++ src/gjitenkai/main.c
@@ -16,8 +16,6 @@ extern void on_gjitenkai_search_expression_activate(GtkEntry *entry,
 extern void on_button_download_clicked(GtkButton* button, worddic *p_worddic);
 
 
-const gchar* home = "style.css";
-
 GError *error = 0;
 
 int main( int argc, char **argv){
@@ -217,14 +215,21 @@ int main( int argc, char **argv){
                    G_CALLBACK(on_gjitenkai_search_expression_activate),
                    &gjitenkai);
 
-  provider = gtk_css_provider_new();
-  display = gdk_display_get_default ();
-  screen = gdk_display_get_default_screen (display);
-  gtk_style_context_add_provider_for_screen (screen,
-					     GTK_STYLE_PROVIDER (provider),
-					     GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+
+
+  //search css
   GError *error = 0;
-  gtk_css_provider_load_from_file(provider, g_file_new_for_path(home), &error);
+  rest = "gjitenkai/style.css";
+  gchar *filename_css = get_file(dirs, rest);
+  if(filename_css){
+    provider = gtk_css_provider_new();
+    display = gdk_display_get_default ();
+    screen = gdk_display_get_default_screen (display);
+    gtk_style_context_add_provider_for_screen (screen,
+					       GTK_STYLE_PROVIDER (provider),
+					       GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+    gtk_css_provider_load_from_file(provider, g_file_new_for_path(filename_css), &error);
+  }
 
   // Initialisation of dictionary available to downlaod url
   rest = g_strjoin(G_DIR_SEPARATOR_S,
