870969f1c4435d334bbce9b62b5305cde6a81bcb
odrevet <olivier.drevet@cycleco.eu>
2017-12-01 16:36:03 +0100

    save extracted file onto the tmp directory

--- src/worddic/worddic_dicfile.c
+++ src/worddic/worddic_dicfile.c
@@ -69,10 +69,11 @@ gboolean worddic_dicfile_open(WorddicDicfile *dicfile, gchar *path){
     unsigned char in[CHUNK];
     unsigned char out[CHUNK];
 
-    //FILE *outfile = fopen("DIC_TMP", "wb");
-    gchar outpath[12] = "DICT_XXXXXX";
-    int fd = mkstemp(outpath);
+    gchar *tmp_dir = g_get_tmp_dir();
+    gchar *outpath = g_strjoin (G_DIR_SEPARATOR_S, tmp_dir, "DICT_XXXXXX", NULL);
+    g_free(tmp_dir);
 
+    int fd = mkstemp(outpath);
     strm.zalloc = Z_NULL;
     strm.zfree = Z_NULL;
     strm.opaque = Z_NULL;
